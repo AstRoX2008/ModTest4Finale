792f04e72b03ffa2f424db4545e4b7f921685a32
AstRoX2008 <malghndwr993@gmail.com>
2026-01-20 20:33:48 +0200

    Changed

---
+++ src/main/java/net/astro142/testmod4finale/item/ModItems.java
@@ -0,0 +1,19 @@
+package net.astro142.testmod4finale.item;
+
+import net.astro142.testmod3.TestMod3;
+import net.astro142.testmod4finale.TestMod4Finale;
+import net.minecraft.world.item.Item;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredItem;
+import net.neoforged.neoforge.registries.DeferredRegister;
+
+public class ModItems {
+    public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(TestMod4Finale.MOD_ID);
+
+    public static final DeferredItem<Item> BLACKPOWDER = ITEMS.register("blackpowder",
+            () -> new Item(new Item.Properties()));
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+}
