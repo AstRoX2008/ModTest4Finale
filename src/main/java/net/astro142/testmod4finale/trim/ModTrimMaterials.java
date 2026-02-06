15b779699396dc60d64b3cee925e262cc1ec1083
AstRoX2008 <malghndwr993@gmail.com>
2026-02-06 22:35:51 +0200

    Changed

---
+++ src/main/java/net/astro142/testmod4finale/trim/ModTrimMaterials.java
@@ -0,0 +1,20 @@
+package net.astro142.testmod4finale.trim;
+
+
+import net.astro142.testmod4finale.TestMod4Finale;
+import net.astro142.testmod4finale.item.ModItems;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.data.worldgen.BootstrapContext;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.Style;
+import net.minecraft.network.chat.TextColor;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.Util;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.equipment.trim.MaterialAssetGroup;
+import net.minecraft.world.item.equipment.trim.TrimMaterial;
+
+public class ModTrimMaterials {
+
+}
---
+++ src/main/java/net/astro142/testmod4finale/trim/ModTrimPatterns.java
@@ -0,0 +1,16 @@
+package net.astro142.testmod4finale.trim;
+
+
+import net.minecraft.core.registries.Registries;
+import net.minecraft.data.worldgen.BootstrapContext;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.Util;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.equipment.trim.TrimPattern;
+import net.neoforged.neoforge.registries.DeferredItem;
+
+public class ModTrimPatterns {
+
+}
